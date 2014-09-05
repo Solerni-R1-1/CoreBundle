c6658d3c9c97a7e7a84be843f5b6a80a439ccc27
Gregg CESARINE <gcesarine@rennes.sii.fr>
2014-09-05 15:55:52 +0200

    Migration db for adding blog to mooc

---
+++ Migrations/drizzle_pdo_mysql/Version20140905151911.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Claroline\CoreBundle\Migrations\drizzle_pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/09/05 03:19:13
+ */
+class Version20140905151911 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_mooc 
+            ADD blog_id INT DEFAULT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE claro_mooc 
+            ADD CONSTRAINT FK_FB43C54EDAE07E97 FOREIGN KEY (blog_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_FB43C54EDAE07E97 ON claro_mooc (blog_id)
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_mooc 
+            DROP FOREIGN KEY FK_FB43C54EDAE07E97
+        ");
+        $this->addSql("
+            DROP INDEX UNIQ_FB43C54EDAE07E97 ON claro_mooc
+        ");
+        $this->addSql("
+            ALTER TABLE claro_mooc 
+            DROP blog_id
+        ");
+    }
+}
