19b62e5a7aa6a5c90934a140ae5f6f58b0a48161
simonvart <simon.vart@exigences.biz>
2015-07-20 10:57:48 +0200

    Footer translation

--- Resources/public/js/cookiewarn/cookiewarn.js
+++ Resources/public/js/cookiewarn/cookiewarn.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -51,22 +51,22 @@ function displayCookieWarning() {
 
     var translator = window.Translator;
     var CGUUrl = jQuery('.js-link_cgu').attr('href') + '#cookies';
-    
+
     var CookieWarnMessage = translator.get('platform:cookie_warn_message');
-    var CookieWarnButtonMore = translator.get('platform:know_more');
-    
-    var warningCookieDOM = jQuery('<div class="cookie-warning-container-wrapper"><div class="container cookie-warning-container"><div class="row cookie-warning-ribbon">' + 
+    var CookieWarnButtonMore = translator.get('platform:know_more_cookies');
+
+    var warningCookieDOM = jQuery('<div class="cookie-warning-container-wrapper"><div class="container cookie-warning-container"><div class="row cookie-warning-ribbon">' +
             '<div class="cookie-warning-text">'
-                + CookieWarnMessage 
+                + CookieWarnMessage
             + '</div>'
-            + '<div class="btn-row"><a href="' 
-                + CGUUrl 
-                + '" class="btn btn-gris">' 
-                + CookieWarnButtonMore 
+            + '<div class="btn-row"><a href="'
+                + CGUUrl
+                + '" class="btn btn-gris">'
+                + CookieWarnButtonMore
             + '</a>'
             + '<button class="btn btn-primary js-warn-cookie-trigger">OK</button>'
             + '</div></div></div>');
-    
+
     jQuery('body')
         .addClass('displayCookieWarning')
         .prepend( warningCookieDOM );
@@ -80,7 +80,7 @@ function displayCookieWarning() {
 // Choose source of acceptance (browser dependant) and check existance
 function isCookieWarningAccepted() {
     if ( supports_local_storage() ) {
-        cookieWarnAccepted = localStorage.getItem('cookieWarnAccepted');       
+        cookieWarnAccepted = localStorage.getItem('cookieWarnAccepted');
     } else {
         cookieWarnAccepted = getCookie( 'cookieWarnAccepted' );
     }
