c2ae8a463f62e632d86637a5e02b57360090a62e
JEREMIE COTINIAUX <jcotiniaux@sii.fr>
2016-03-23 12:19:02 +0100

    evolution notification 20

--- Migrations/pdo_mysql/Version20160302182945.php
+++ Migrations/pdo_mysql/Version20160302182945.php
@@ -29,14 +29,6 @@ class Version20160302182945 extends AbstractMigration
             ADD notifcitermooc TINYINT(1) NOT NULL DEFAULT 0,
             ADD notiflikemooc TINYINT(1) NOT NULL DEFAULT 0
         ");
-        $this->addSql(" UPDATE claro_user SET notifarticle=0;
-        ");
-        $this->addSql(" UPDATE claro_user SET notifsujettheme=0;
-        ");
-        $this->addSql(" UPDATE claro_user SET notifciter=0;
-        ");
-        $this->addSql(" UPDATE claro_user SET notiflike=0;
-        ");
     }
 
 
